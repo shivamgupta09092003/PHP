dad0c2de9a9871098c8ae1bd3a68462cc758f841
shivamgupta09092003 <162910657+shivamgupta09092003@users.noreply.github.com>
2024-03-12 23:09:20 +0530

    Add files via upload

---
+++ data2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Submit</title>
+</head>
+<body>
+    <h1> Welcome </h1>
+    <h2>
+        <?php echo $_POST["name"];?>
+</h2>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> My Webpage </title>
+</head>
+<body>
+    <form action= "data2.php" method="POST">
+        <h1>
+            Registration form
+</h1>
+<h1>
+    Enter your name: <br> </h1>
+    <input source="text" name="name" placeholder="Enter the name"> <br>
+    <input type="Submit" value="Submit">
+</form>   
+</body>
+</html>
