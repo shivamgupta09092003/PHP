6cc8f78aed9fa4cac0533d02ec04b5968f4b0f57
shivamgupta09092003 <162910657+shivamgupta09092003@users.noreply.github.com>
2024-03-12 23:38:54 +0530

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action = "data3.php" method="POST">
+        <h1> Registration form </h1>
+        Enter your name: <br>
+        <input source ="text" name="name" placeholder="Enter the name"> <br>
+        Enter your number: <br>
+        <input source ="number" name="number" placeholder="Enter the number"> <br>
+        <input type = "submit" value = "submit">
+</body>
+</html>
