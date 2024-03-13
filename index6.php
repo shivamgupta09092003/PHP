ca452e7d6da53da0dfa78a1360932524236584c4
shivamgupta09092003 <162910657+shivamgupta09092003@users.noreply.github.com>
2024-03-13 21:20:29 +0530

    Add files via upload

---
+++ index6.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="maximum(index6).php" method="post">
+  <input type="number" name="num1" placeholder="Enter the first number">
+  <input type="number" name="num2" placeholder="Enter the second number">
+  <input type="number" name="num3" placeholder="Enter the third number">
+  <input type="submit" value="Find Maximum">
+</form>
+</body>
+</html>
