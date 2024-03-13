cc1168e44776c70cbd74ed3789205cf3f92e9cfc
shivamgupta09092003 <162910657+shivamgupta09092003@users.noreply.github.com>
2024-03-13 21:58:02 +0530

    Multiplication Table

--- Multiplication Table.php
+++ Multiplication Table.php
@@ -1,3 +1,4 @@
+#index.php
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,5 +13,30 @@
         action="tables(index5).php">
         <input type="number" name="table" placeholder="Enter the first number">
         <button type="submit" name="submit">Submit</button>
+</body>
+</html>
+
+#data.php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    if(isset($_POST['submit'])){
+        if(!empty($_POST['table'])){
+            $table=$_POST['table'];
+            for($i=1;$i<=10;$i++){
+                $result=$table*$i;
+                echo $table . "x" . $i . "=" . $result . "<br>";
+            }
+        }
+    }
+    ?>
 </body>
 </html>
