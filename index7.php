95b02984d80b50c49e406d2e4df84bd84b9f72cb
shivamgupta09092003 <162910657+shivamgupta09092003@users.noreply.github.com>
2024-03-19 19:41:04 +0530

    Add files via upload

---
+++ index7.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buzz Fizz</title>
+</head>
+<body>
+    <pre>
+    <?php
+    for ($i=1; $i <100 ; $i++) { 
+        if ($i%3==0 || $i%5==0){
+            if ($i%3==0){
+                echo "FIZZ"." ";
+            }
+            if ($i%5==0) {
+                echo "BUZZ"." ";
+            }
+        }
+        else{
+            echo $i." ";
+        }
+    }
+    ?>
+    </pre>
+</body>
+</html>
