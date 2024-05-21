bc7ff47daff37c9e517b6e8fa6f9d521cd822be3
shivamgupta09092003 <162910657+shivamgupta09092003@users.noreply.github.com>
2024-05-21 10:42:48 +0530

    Self-Processing.php

--- Self-Processing.php
+++ Self-Processing.php
@@ -1,26 +1,31 @@
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    
-   ?>
-    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name"><br><br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email"><br><br>
-        <input type="submit" value="Submit">
-    </form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="  Self-processing.php" method="post">
+            <input type="text" name="name" placeholder="name">
+            <input type="email" name="email" placeholder="email"> <!-- added placeholder -->
+            <input type="submit" name="submit" value="submit"> <!-- added name="submit" -->
+        </form>
     <?php
-} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] === "GET"){
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    
-    if (empty($name) || empty($email)) {
-        echo "Please fill in all fields.";
-    } else {
-        
-        echo "Thank you, $name, for submitting your email address: $email.";
+        echo"<p>get method!!</p>";
+    }else{
+        if(isset($_POST['submit'])){
+            $name = isset($_POST['name']) ? $_POST['name'] : ''; // handling if name is not set
+            $email = isset($_POST['email']) ? $_POST['email'] : ''; // handling if email is not set
+            echo $name." ".$email;
+        }
+        // else{
+          
+        // }
     }
-}
-?>
+    ?>
+    
+</body>
+</html>
