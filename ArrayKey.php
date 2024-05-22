89e193ca5097e569e052d27c81ed8f718f2fbbb2
shivamgupta09092003 <162910657+shivamgupta09092003@users.noreply.github.com>
2024-05-22 23:06:30 +0530

    ArrayKey.php

--- ArrayKey.php
+++ ArrayKey.php
@@ -1,27 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Get Array Key</title>
+</head>
+<body>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    Enter a value: <input type="number" name="value" min="1" max="5" required><br><br>
+    <input type="submit" name="submit" value="Get Key">
+</form><br><br>
+
 <?php
+$array = array("one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5);
 
-$arrayList = array(
-    'fruit' => 'apple',
-    'animal' => 'dog',
-    'city' => 'new york',
-    'car' => 'toyota'
-);
-
-
-function getArrayKey(array $array, $searchValue) {
-    return array_search($searchValue, $array);
-}
-
-
-$searchValue = 'dog';
-
-
-$key = getArrayKey($arrayList, $searchValue);
-
-
-if ($key!== false) {
-    echo "The key of '$searchValue' is '$key'.";
-} else {
-    echo "The value '$searchValue' is not found in the array.";
+if(isset($_POST['submit'])){
+    $value = $_POST['value'];
+    $key = array_search($value, $array);
+    if($key!== false){
+        echo "The key for the value {$value} is: {$key}";
+    }else{
+        echo "The value {$value} was not found in the array.";
+    }
 }
 ?>
+
+
+
+</body>
+</html>
