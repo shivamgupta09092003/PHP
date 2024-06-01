1e078d694e93b813d4cfa77b99ef8fa83c57a0e3
shivamgupta09092003 <162910657+shivamgupta09092003@users.noreply.github.com>
2024-06-01 18:45:52 +0530

    Add files via upload

---
+++ Page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+    <style>
+        body {
+            font-family:Arial, Helvetica, sans-serif;
+            margin: 50;
+            padding: 50;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 0;
+            text-align: center;
+        }
+        nav {
+            text-align:center;
+        }
+        nav a {
+            color: #fff;
+            padding: 10px;
+            text-decoration: none;
+        }
+        nav a:hover {
+            background-color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>My Website</h1>
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="about1.php">About</a>
+        </nav>
+    </header>
+</body>
+</html>
